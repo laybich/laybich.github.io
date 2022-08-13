53027bcf21bd2cf9d48716224284c8c6978da12a
Matthew Sai <super.matvij@gmail.com>
2022-08-14 00:03:06 +0300

    fixed main menu

--- src/components/UI/navicon/NavIcon.jsx
+++ src/components/UI/navicon/NavIcon.jsx
@@ -3,12 +3,12 @@ import './NavIcon.css';
 
 function NavIcon() {
 	return (
-		<div>
+		<>
 			<input className="menu-btn" type='checkbox' id='menu-btn' />
 			<label className='menu-icon' htmlFor="menu-btn">
 				<span className="navicon" />
 			</label>
-		</div>
+		</>
 	);
 }
 
