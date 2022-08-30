5b2f8fdc00acc5311d0542014e6148a9fd8622f1
Matthew Sai <super.matvij@gmail.com>
2022-08-31 00:43:11 +0300

    Update social medias

--- src/portfolio.js
+++ src/portfolio.js
@@ -27,7 +27,7 @@ const socialMediaLinks = [
 	},
 	{
 		name: 'LinkedIn',
-		link: 'https://www.linkedin.com/in/laybich/',
+		link: 'https://ua.linkedin.com/in/laybich-inc-647225238/',
 		fontAwesomeIcon: 'fa-linkedin-in', // Reference https://fontawesome.com/icons/linkedin-in?style=brands
 		backgroundColor: '#0077B5', // Reference https://simpleicons.org/?q=linkedin
 	},
